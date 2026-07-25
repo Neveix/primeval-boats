e059149927d860433b79e8043afcb059c7e7edce
neveix <i@neveix.ru>
2026-07-25 17:33:00 +0500

    move logic to vehicle_api

---
+++ modules/vehicle_api/boat/log_raft.lua
@@ -0,0 +1,15 @@
+return {
+	get = function()
+		return {
+			layout_id = "primeval_boats:inventory60",
+			inventory_size = 60,
+			gravity = 1,
+			max_speed = 5,
+			rotation_acceleration = 0.02,
+			rotation_deceleration = 0.01,
+			turn_velocity_dependency = 0,
+			roll_lift = 0,
+			bottom_y_shift = -0.2,
+		}
+	end,
+}
--- scripts/kayak.item.lua
+++ scripts/kayak.item.lua
@@ -1,20 +1,20 @@
 local PACK_ID = "primeval_boats"
-local boat_item_common = require(PACK_ID .. ":boat_item_common")
+local boat_utils = require("vehicle_api:boat_utils")
 
 local ENTITY_ID = PACK_ID .. ":kayak"
 
 function on_use(pid)
-	if boat_item_common.placement_mutex.try_lock() == false then
+	if boat_utils.placement_mutex.try_lock() == false then
 		return
 	end
-	local success, pos = boat_item_common.get_boat_placement_by_player(pid, entities.def_index(ENTITY_ID))
+	local success, pos = boat_utils.get_boat_placement_by_player(pid, entities.def_index(ENTITY_ID))
 	if not success then
 		return
 	end
-	boat_item_common.try_delete_item(pid)
-	local rot = boat_item_common.get_player_rotation_mat4(pid)
+	boat_utils.try_delete_item(pid)
+	local rot = boat_utils.get_boat_rotation_mat4(pid)
 	entities.spawn(ENTITY_ID, pos, {
-		[PACK_ID .. "__boat"] = {
+		["vehicle_api__boat"] = {
 			rotation_mat4 = rot,
 		},
 	})
