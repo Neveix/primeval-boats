0feaa76f8dff4e7259e913d840adfe4b8c27abcb
neveix <i@neveix.ru>
2026-07-26 00:35:57 +0500

    modify to match new api of vehicle_api

--- modules/vehicle_api/boat/kayak.lua
+++ modules/vehicle_api/boat/kayak.lua
@@ -1,16 +1,12 @@
 return {
-	get = function()
-		return {
-			inventory_size = 0,
-			gravity = 0.8,
-			max_speed = 10,
-			acceleration = 0.07,
-			rotation_acceleration = 0.03,
-			max_rotation_speed = 1.0,
-			turn_velocity_dependency = 0.4,
-			bottom_y_shift = -0.125,
-			roll_lift = 0.0625,
-			max_roll = 5,
-		}
-	end,
+	inventory_size = 0,
+	gravity = 0.8,
+	max_speed = 10,
+	acceleration = 0.07,
+	rotation_acceleration = 0.03,
+	max_rotation_speed = 1.0,
+	turn_velocity_dependency = 0.4,
+	bottom_y_shift = -0.125,
+	roll_lift = 1 / 16,
+	max_roll = 5,
 }
--- modules/vehicle_api/boat/plank_boat.lua
+++ modules/vehicle_api/boat/plank_boat.lua
@@ -1,17 +1,13 @@
 return {
-	get = function()
-		return {
-			layout_id = "primeval_boats:inventory20",
-			inventory_size = 20,
-			gravity = 1,
-			max_speed = 7,
-			rotation_acceleration = 0.01,
-			rotation_deceleration = 0.002,
-			max_rotation_speed = 0.8,
-			turn_velocity_dependency = 0.6,
-			bottom_y_shift = -0.11,
-			roll_lift = 0.125,
-			max_roll = 15,
-		}
-	end,
+	layout_id = "primeval_boats:inventory20",
+	inventory_size = 20,
+	gravity = 1,
+	max_speed = 7,
+	rotation_acceleration = 0.01,
+	rotation_deceleration = 0.002,
+	max_rotation_speed = 0.8,
+	turn_velocity_dependency = 0.6,
+	bottom_y_shift = -0.11,
+	roll_lift = 2 / 16,
+	max_roll = 15,
 }
